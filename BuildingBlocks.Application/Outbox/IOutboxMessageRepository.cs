c3b9789940eecb3430434b3b503da0662094de55
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-03-09 17:48:17 +0200

    Adding BuildingBlocks.Application proj

---
+++ BuildingBlocks.Application/Outbox/IOutboxMessageRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Application.Outbox;
+
+public interface IOutboxMessageRepository
+{
+    Task<List<OutboxMessage>> GetUnprocessedMessagesAsync();
+
+    public Task SaveChangesAsync();
+}
