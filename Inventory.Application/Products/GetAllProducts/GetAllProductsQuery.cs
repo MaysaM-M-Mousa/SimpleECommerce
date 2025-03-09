847119d3d66cf7b6522ac328c1a8e33024fe54e8
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-03-10 00:47:24 +0200

    Moving GetAllProductsQueryHandler to its own file

--- Inventory.Application/Products/GetAllProducts/GetAllProductsQuery.cs
+++ Inventory.Application/Products/GetAllProducts/GetAllProductsQuery.cs
@@ -4,18 +4,3 @@ using MediatR;
 namespace Inventory.Application.Products.GetAllProducts;
 
 public record GetAllProductsQuery : IRequest<List<Product>>;
-
-internal class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, List<Product>>
-{
-    private readonly IProductRepository _productRepository;
-
-    public GetAllProductsQueryHandler(IProductRepository productRepository)
-    {
-        _productRepository = productRepository;
-    }
-
-    public async Task<List<Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
-    {
-        return await _productRepository.GetAllProductsAsync();
-    }
-}
---
+++ Inventory.Application/Products/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain.Products;
+using MediatR;
+
+namespace Inventory.Application.Products.GetAllProducts;
+
+internal class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, List<Product>>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetAllProductsQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<List<Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+    {
+        return await _productRepository.GetAllProductsAsync();
+    }
+}
