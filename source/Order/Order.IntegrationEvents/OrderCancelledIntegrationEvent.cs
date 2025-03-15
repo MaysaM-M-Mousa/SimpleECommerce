be0aca82b5d7a6aded0e5bef3f38a6d8bc714d14
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-03-15 17:04:15 +0200

    Adding OrderCancelledIntegrationEvent

---
+++ source/Order/Order.IntegrationEvents/OrderCancelledIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.IntegrationEvent;
+
+namespace Order.IntegrationEvents;
+
+public class OrderCancelledIntegrationEvent : IntegrationEvent
+{
+    public Guid OrderId { get; }
+
+    public string? Reason { get; }
+
+    public bool IsPlaced { get; }
+
+    public List<Item> Items { get; } = new();
+
+    public DateTime OccurredOnUtc { get; set; }
+}
