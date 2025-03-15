ae7cefd5697ae581379e1390221a086609797081
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-03-15 17:43:40 +0200

    Adding `Create` use case tests

---
+++ source/Order/Tests/Order.Domain.Tests/Orders/CreateTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Order.Domain.Orders.Events;
+
+namespace Order.Domain.Tests.Orders;
+
+public class CreateTests
+{
+    [Fact]
+    public void CreateNewOrder_Succeeds()
+    {
+        // Arrange
+        string description = "Order 1 - description";
+        Guid customerId = new Guid("70b4c215-9139-487d-8502-670953cd9107");
+
+        // Act
+        var order = Domain.Orders.Order.Create(description, customerId);
+
+        // Assert
+        using (new AssertionScope())
+        {
+            order.Should().NotBeNull();
+            order.CustomerId.Should().Be(customerId);
+            order.Description.Should().Be(description);
+            order.GetDomainEvents().Count.Should().Be(1);
+            order.GetDomainEvents().First().Should().BeOfType<OrderCreatedDomainEvent>();
+        }
+    }
+}
