1f34445be7e8723c9ca6e643d634189b0dc54f76
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-03-23 00:51:30 +0200

    Adding CalculateTotalPrice tests

---
+++ source/Order/Tests/Order.Domain.Tests/LineItems/CalculateTotalPriceTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Order.Domain.Orders;
+
+namespace Order.Domain.Tests.LineItems;
+
+public class CalculateTotalPriceTests
+{
+    [Fact]
+    public void CalculateTotalPrice_ReturnsCorrectValue()
+    {
+        // Arrange
+        var lineItem = LineItem.Create(id: Guid.NewGuid(), productId: 1, price: 50.5m, quantity: 2, orderId: Guid.NewGuid());
+
+        // Act
+        var totalPrice = lineItem.CalculateTotalPrice();
+
+        // Assert
+        totalPrice.Should().Be(50.5m * 2);
+    }
+}
