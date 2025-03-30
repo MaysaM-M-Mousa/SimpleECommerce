75c377848e37049564845adaa88baf381731f3ca
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-03-31 02:44:23 +0300

    introducing Reservation entity in Product aggregate

---
+++ source/Inventory/Inventory.Domain/Products/Reservation.cs
@@ -0,0 +1,27 @@
+﻿namespace Inventory.Domain.Products;
+
+public class Reservation
+{
+    public long Id { get; private set; }
+
+    public int ProductId { get; private set; }
+
+    public Guid OrderId { get; private set; }
+
+    public int Quantity { get; private set; }
+
+    private Reservation()
+    {
+        // Only EF Core
+    }
+
+    private Reservation(int productId, Guid orderId, int quantity)
+    {
+        ProductId = productId;
+        OrderId = orderId;
+        Quantity = quantity;
+    }
+
+    public static Reservation Create(int productId, Guid orderId, int quantity)
+        => new Reservation(productId, orderId, quantity);
+}
