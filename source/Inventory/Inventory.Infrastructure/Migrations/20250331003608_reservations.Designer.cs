4fdf19bb5a776d35a0ab5b836d663e67fee91ff8
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-03-31 03:38:56 +0300

    Adding Reservation's entity configuration

--- source/Inventory/Inventory.Infrastructure/Migrations/20250331003608_reservations.Designer.cs
+++ source/Inventory/Inventory.Infrastructure/Migrations/20250331003608_reservations.Designer.cs
@@ -4,6 +4,7 @@ using Inventory.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Inventory.Infrastructure.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250331003608_reservations")]
+    partial class reservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ source/Inventory/Inventory.Infrastructure/Persistence/EntityConfigurations/ReservationEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory.Infrastructure.Persistence.EntityConfigurations;
+
+internal class ReservationEntityConfiguration : IEntityTypeConfiguration<Reservation>
+{
+    public void Configure(EntityTypeBuilder<Reservation> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder
+            .HasOne<Product>()
+            .WithMany(x => x.Reservations)
+            .HasForeignKey(x => x.ProductId)
+            .IsRequired();
+    }
+}
