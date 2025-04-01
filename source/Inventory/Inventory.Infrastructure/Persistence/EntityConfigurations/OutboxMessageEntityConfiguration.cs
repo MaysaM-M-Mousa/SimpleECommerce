c5b159e8ea7b35ac196831925f11fb84c8ab9b38
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-04-01 04:12:02 +0300

    Adding OutboxMessage entity config and its migration in inventory service

---
+++ source/Inventory/Inventory.Infrastructure/Persistence/EntityConfigurations/OutboxMessageEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.Application.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory.Infrastructure.Persistence.EntityConfigurations;
+
+internal class OutboxMessageEntityConfiguration : IEntityTypeConfiguration<OutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.HasIndex(e => e.ProcessedOnUtc);
+    }
+}
