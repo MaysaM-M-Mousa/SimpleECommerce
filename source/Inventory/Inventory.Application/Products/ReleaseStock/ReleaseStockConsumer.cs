46d4b2a6c4c0edc6c51574d5a868f97a9c0c060d
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-04-02 00:32:32 +0300

    Adding Reserve and Release stock masstransit consumers

---
+++ source/Inventory/Inventory.Application/Products/ReleaseStock/ReleaseStockConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Application.Products.ReserveStock.Saga;
+using MassTransit;
+using MediatR;
+
+namespace Inventory.Application.Products.ReleaseStock;
+
+public class ReleaseStockConsumer : IConsumer<ReleaseStockRequest>
+{
+    private readonly IMediator _mediator;
+
+    public ReleaseStockConsumer(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task Consume(ConsumeContext<ReleaseStockRequest> context)
+    {
+        var request = context.Message;
+
+        await _mediator.Send(new ReleaseStockCommand(request.ProductId, request.Quantity, request.OrderId));
+    }
+}
---
+++ source/Inventory/Inventory.Application/Products/ReserveStock/ReserveStockConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Application.Products.ReserveStock.Saga;
+using MassTransit;
+using MediatR;
+
+namespace Inventory.Application.Products.ReserveStock;
+
+public class ReserveStockConsumer : IConsumer<ReserveStockRequest>
+{
+    private readonly IMediator _mediator;
+
+    public ReserveStockConsumer(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task Consume(ConsumeContext<ReserveStockRequest> context)
+    {
+        var request = context.Message;
+
+        await _mediator.Send(new ReserveStockCommand(request.ProductId, request.Quantity, request.OrderId));
+    }
+}
