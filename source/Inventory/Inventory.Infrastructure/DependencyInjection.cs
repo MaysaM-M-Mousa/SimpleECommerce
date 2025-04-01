0674e8852dda78500d401fc61d7f6e9ba1af8a59
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-04-01 04:02:58 +0300

    Adding outbox interceptor for inventory service

--- source/Inventory/Inventory.Infrastructure/DependencyInjection.cs
+++ source/Inventory/Inventory.Infrastructure/DependencyInjection.cs
@@ -3,6 +3,7 @@ using BuildingBlocks.Application.Inbox;
 using Inventory.Domain.Products;
 using Inventory.Infrastructure.BackgroundJobs;
 using Inventory.Infrastructure.Persistence;
+using Inventory.Infrastructure.Persistence.Interceptors;
 using Inventory.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -20,12 +21,17 @@ public static class DependencyInjection
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IInboxRepository, InboxRepository>();
         services.AddScoped<IDatabaseTransaction, DatabaseTransaction>();
+        services.AddSingleton<OutboxInterceptor>();
 
-        services.AddDbContext<InventoryDbContext>(
-            opts => opts.UseSqlServer(configuration.GetConnectionString("InventoryDbConnectionString"), builder =>
+        services.AddDbContext<InventoryDbContext>((sp, options) =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("InventoryDbConnectionString"), builder =>
             {
                 builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(5), null);
-            }));
+            });
+            options.AddInterceptors(sp.GetRequiredService<OutboxInterceptor>());
+        });
+
 
         services.AddQuartz(config =>
         {
---
+++ source/Inventory/Inventory.Infrastructure/Persistence/Interceptors/OutboxInterceptor.cs
@@ -0,0 +1,49 @@
+﻿using BuildingBlocks.Application.Outbox;
+using BuildingBlocks.Domain;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Text.Json;
+
+namespace Inventory.Infrastructure.Persistence.Interceptors;
+
+internal class OutboxInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        var dbContext = eventData.Context;
+
+        if (dbContext is null)
+        {
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        var events = dbContext
+            .ChangeTracker
+            .Entries<AggregateRoot>()
+            .Select(e => e.Entity)
+            .SelectMany(aggregateRoot =>
+            {
+                var domainEvents = aggregateRoot.GetDomainEvents();
+
+                aggregateRoot.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        var outboxMessages = events
+            .Select(e => new OutboxMessage()
+            {
+                Id = e.Id,
+                OccurredOnUtc = DateTime.UtcNow,
+                Type = e.GetType().AssemblyQualifiedName,
+                Content = JsonSerializer.Serialize(e, e .GetType())
+            }).ToList();
+
+        dbContext.Set<OutboxMessage>().AddRange(outboxMessages);
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
