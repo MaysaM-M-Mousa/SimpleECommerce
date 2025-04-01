85fef8da17791cd98b800de8edcc556585411d09
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-04-01 04:36:03 +0300

    Adding release & reserve domain event handlers

---
+++ source/Inventory/Inventory.Application/Products/ReleaseStock/StockReleasedDomainEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using Inventory.Domain.Products.DomainEvents;
+using Inventory.IntegrationEvents;
+using MassTransit;
+using MediatR;
+
+namespace Inventory.Application.Products.ReleaseStock;
+
+internal class StockReleasedDomainEventHandler : INotificationHandler<StockReleasedDomainEvent>
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public StockReleasedDomainEventHandler(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    public async Task Handle(StockReleasedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        await _publishEndpoint.Publish(new StockReleasedIntegrationEvent
+        {
+            MessageId = notification.Id,
+            ProductId = notification.ProductId,
+            Quantity = notification.Quantity,
+            OrderId = notification.OrderId,
+            OccurredOnUtc = notification.OccurredOn
+        });
+    }
+}
---
+++ source/Inventory/Inventory.Application/Products/ReserveStock/StockReservedDomainEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using Inventory.Domain.Products.DomainEvents;
+using Inventory.IntegrationEvents;
+using MassTransit;
+using MediatR;
+
+namespace Inventory.Application.Products.ReserveStock;
+
+internal class StockReservedDomainEventHandler : INotificationHandler<StockReservedDomainEvent>
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public StockReservedDomainEventHandler(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    public async Task Handle(StockReservedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        await _publishEndpoint.Publish(new StockReservedIntegrationEvent
+        {
+            MessageId = notification.Id,
+            ProductId = notification.ProductId,
+            Quantity = notification.Quantity,
+            OrderId = notification.OrderId,
+            OccurredOnUtc = notification.OccurredOn
+        });
+    }
+}
