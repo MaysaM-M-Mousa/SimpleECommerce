7b629d9789b3ff23434e6a00bfb5ebe1e7bcf34c
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2025-04-02 20:52:40 +0300

    adding entity configuration for saga state class

---
+++ source/Inventory/Inventory.Infrastructure/Persistence/EntityConfigurations/ReserveStocksSagaStateEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Inventory.Application.Products.ReserveStock.Saga;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory.Infrastructure.Persistence.EntityConfigurations;
+
+internal class ReserveStocksSagaStateEntityConfiguration : IEntityTypeConfiguration<ReserveStocksSagaState>
+{
+    public void Configure(EntityTypeBuilder<ReserveStocksSagaState> builder)
+    {
+        builder.HasKey(x => x.CorrelationId);
+
+        builder.Property(x => x.CurrentState)
+               .IsRequired();
+
+        builder.Property(x => x.OrderId)
+               .IsRequired();
+
+        builder.OwnsOne(s => s.ReservationDetails, d =>
+        {
+            d.ToJson();
+            d.OwnsMany(x => x.ProductsToReserve);
+            d.OwnsMany(x => x.ReservedProducts);
+            d.OwnsMany(x => x.ProductsToRelease);
+        });
+    }
+}
